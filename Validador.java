2f5f01d1b22cb7985f5c9131f9b5f0496e3b1da7
João <joaosantanaguilherme@gmail.com>
2026-06-16 20:19:29 -0300

    Base da API e esteira de CI

---
+++ Validador.java
@@ -0,0 +1,19 @@
+public class Validador {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Inicializando verificação...");
+
+        boolean bancoPonto = true;
+
+        if(bancoPonto){
+            System.out.println("SISTEMA OK!!! SUCESSO!");
+        }else{
+            System.out.println("SISTEMA DEU RUIM");
+            System.exit(1);
+
+        }
+
+
+    }
+}
